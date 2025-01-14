1d0b105ba5197edee0a1152df72916f05a912bcb
Jakub Slambora <jakub@oakslab.com>
2025-01-14 11:19:18 +0100

    Streaming & tool calling

--- main.ts
+++ main.ts
@@ -1,12 +1,12 @@
 // Import necessary modules
 import { ChatOpenAI } from "@langchain/openai";
-import { createReadStream } from "fs";
-import { Readable } from "stream";
 import * as dotenv from "dotenv";
-import { HumanMessage } from "@langchain/core/messages";
+import {
+  AIMessageChunk,
+  BaseMessage,
+  HumanMessage,
+} from "@langchain/core/messages";
 import { SystemMessage } from "@langchain/core/messages";
-import { ChatPromptTemplate } from "@langchain/core/prompts";
-import { JsonOutputFunctionsParser } from "langchain/output_parsers";
 import { tool } from "@langchain/core/tools";
 import { z } from "zod";
 import { concat } from "@langchain/core/utils/stream";
@@ -48,51 +48,69 @@ const calculatorTool = tool(
   }
 );
 
-// Function to stream the output from the LLM
-async function streamLLMOutput() {
-  // Initialize the ChatOpenAI instance
-  const chat = new ChatOpenAI({
-    apiKey: process.env.OPENAI_API_KEY,
-    model: "gpt-4",
-    temperature: 0,
-  }).bindTools([calculatorTool]);
+// Initialize the ChatOpenAI instance
+const chat = new ChatOpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+  model: "gpt-4",
+  temperature: 0,
+}).bindTools([calculatorTool]);
 
-  // Create prompt template for calculator
+async function* agentStream(messages: BaseMessage[]) {
+  // Call the LLM with function calling
+  let stream = await chat.stream(messages);
+
+  let toolCallMessage: AIMessageChunk | undefined = undefined;
+  for await (const chunk of stream) {
+    // tool chunks are there only when the LLM is calling the tool
+    if (chunk.tool_call_chunks) {
+      // concat the tool call message with the current chunk
+      toolCallMessage =
+        toolCallMessage !== undefined ? concat(toolCallMessage, chunk) : chunk;
+      console.log("tool_call_chunks", chunk.tool_call_chunks);
+    }
+
+    // stream the user-facing chunk
+    if (chunk.content) {
+      yield chunk.content;
+    }
+  }
+
+  // if there is concatenated tool call message, we can call the tool
+  if (toolCallMessage) {
+    console.log("toolCallMessage", toolCallMessage);
+    const toolResultMessage = await calculatorTool.invoke(
+      toolCallMessage!.tool_calls![0]
+    );
+    console.log("toolResultMessage", toolResultMessage);
+    // stream the final response from LLM
+    stream = await chat.stream([
+      ...messages,
+      toolCallMessage,
+      toolResultMessage,
+    ]);
+
+    for await (const chunk of stream) {
+      // stream the final response from LLM
+      if (chunk.content) {
+        yield chunk.content;
+      }
+    }
+  }
+}
+
+async function main() {
   const messages = [
     new SystemMessage(
       `
       You are a helpful math assistant. Use the calculator function to solve math problems. Before calling the calculator function, let the user know that you are going to use the calculator function.
-
-      Example output:
-      User: What is 25 multiplied by 12?
-      Assistant: I'm going to use the calculator function to solve this problem.
-      Calculator: 25 * 12 = 300
       `
     ),
     new HumanMessage("What is 25 multiplied by 12?"),
   ];
 
-  // Call the LLM with function calling
-  const toolCallMessage = await chat.invoke(messages);
-
-  const toolResultMessage = await calculatorTool.invoke(
-    toolCallMessage.tool_calls![0]
-  );
-
-  console.log(toolResultMessage);
-
-  const response = await chat.invoke([
-    ...messages,
-    toolCallMessage,
-    toolResultMessage,
-  ]);
-
-  console.log(response);
-}
-
-// Main function to run the script
-async function main() {
-  await streamLLMOutput();
+  for await (const chunk of agentStream(messages)) {
+    console.log("User-facing chunk:", chunk);
+  }
 }
 
 // Run the main function
